4cdf92ea074d954d76a47584844cee788e5d73c5
«99Pedro» <p.guehe99@gmail.com>
2025-12-19 12:18:35 +0100

    subiendo la base de datos

--- src/BBDD/BBDD.java
+++ src/BBDD/BBDD.java
@@ -1,1 +1,23 @@
- 
+package BBDD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class BBDD {
+    public void conectarBBDD () {
+        String user = "root";
+        String password = "1234";
+
+        String url = "jdbc:mysql://localhost:3307/roguelitee";
+
+        try (Connection conexion = DriverManager.getConnection(url, user, password)) {
+            Statement statement = conexion.createStatement();
+            {
+
+            }
+        } catch (Exception e) {
+
+        }
+    }
+}
