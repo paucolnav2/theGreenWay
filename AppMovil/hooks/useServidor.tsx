0f6abcdb5a6e94f352a2e1bde7658ed8fca59974
sercanmar <sercanmar@alu.edu.gva.es>
2026-01-22 12:03:52 +0100

    Socket TCP restaurado

--- AppMovil/hooks/useServidor.tsx
+++ AppMovil/hooks/useServidor.tsx
@@ -1,38 +1,36 @@
+import TcpSocket from 'react-native-tcp-socket';
 
 export function useServidor() {
   
+  const IP_SERVIDOR = "10.0.2.2"; 
+  const PUERTO = 8080;
 
-  const IP_SERVIDOR = "172.30.77.43"; 
-  const PUERTO = "8080";
+  const enviarCoordenadas = (latitud: any, longitud: any, idUsuario: any) => {
+    const datos = {
+      lat: latitud,
+      lon: longitud,
+      userId: parseInt(idUsuario)
+    };
 
-  const enviarCoordenadas = async (latitud: any, longitud: any, idUsuario: any) => {
-    try {
- 
-      const datos = {
-        lat: latitud,
-        lon: longitud,
-        userId: parseInt(idUsuario)
-      };
+    const jsonEnviar = JSON.stringify(datos);
+    console.log("enviando por tcp...", jsonEnviar);
+    const client = TcpSocket.createConnection({
+      port: PUERTO,
+      host: IP_SERVIDOR,
+    }, () => {
+      client.write(jsonEnviar);
+      client.end();
+    });
 
-      console.log("enviando a java...", datos);
-      
-      await fetch(`http://${IP_SERVIDOR}:${PUERTO}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(datos),
-      });
-      
-      console.log("recibido por el servidor!");
+    client.on('error', (error) => {
+      console.log("fallo algo en el socket", error);
+    });
 
-    } catch (error) {
-    
-      console.log("error de conex");
-    }
+    client.on('close', () => {
+      console.log("conexion cerrada");
+    });
   };
 
- 
   return {
     enviarCoordenadas
   };
